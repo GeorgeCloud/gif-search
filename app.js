6f3277bd1e66ae74b06dfdff824f1383eba642c9
GeorgeCloud <net.george.sky@gmail.com>
2022-03-24 19:10:55 -0700

    add: request and results show page for web app, styling

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const express = require('express');
-
 const app = express();
+
+require('dotenv').config();
+
 const { engine } = require('express-handlebars');
+const fetch = require('node-fetch');
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 app.set("views", "./views");
+
+function build_tenor_url(query){
+  return `https://g.tenor.com/v1/search?q=${query.replace(/\s+/g, '-')}&key=${process.env.API_KEY}&limit=8`
+}
 
 app.get('/',
   (req, res) => {
-    res.render('home');
-  }
-);
-
-app.get('/hello/:name',
-  (req, res) => {
-    const name = req.params.name;
-    res.send(`Hello ${name}!`);
+    const gifInput = req.query.gifInput;
+    if (gifInput) {
+      const tenorUrl = build_tenor_url(gifInput);
+      fetch(tenorUrl)
+      .then(response => response.json())
+      .then(
+        (data) => {
+          const gifs = data.results;
+          res.render('home', { gifs })
+        }
+      );
+    }
+    else {
+      res.render('home');
+    }
   }
 );
 
